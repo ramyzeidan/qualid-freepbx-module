ac058c80e8dfad113ddebb16c24f375acc2df907
Ramy Zeidan <ramyblazin@gmail.com>
2026-08-05 07:19:10 +0300

    Fix install.php for PHP 5.6

--- qualid_remote/install.php
+++ qualid_remote/install.php
@@ -6,25 +6,6 @@
 
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
-// Set default config values on fresh install
-$defaults = [
-    'qualid_api_key'      => '',
-    'qualid_company_id'   => '',
-    'qualid_sip_domain'   => '',
-    'qualid_trunk_user'   => '',
-    'qualid_trunk_pass'   => '',
-    'qualid_turn_server'  => '',
-    'qualid_connected'    => '0',
-    'qualid_connected_at' => '',
-    'qualid_last_error'   => '',
-];
-
-foreach ($defaults as $key => $val) {
-    if (getConfig($key) === false) {
-        setConfig($key, $val);
-    }
-}
-
 // Ensure the Asterisk config directory is writable
 $dirs = ['/etc/asterisk'];
 foreach ($dirs as $dir) {
